08176a0b8ecca7df3fd481e35beac72db0a00279
Tatiana Daza <147081026+TatianaDaza@users.noreply.github.com>
2025-09-05 16:03:00 -0500

    ..

--- Estructura de datos no lineales/listadoblementeenlazada.py
+++ Estructura de datos no lineales/listadoblementeenlazada.py
@@ -6,30 +6,58 @@ class Nodo:
 
 class pilaDoblementeEnlazada:
     def __init__(self):
-        self.tope = None
+        self.frente= None
+        self.ultimo =None
         self.tamaño = 0
 
-    def push(self, dato):
+    def insert_front(self, dato):
         nuevo = Nodo(dato)
-        if self.tope:
-            nuevo.siguiente = self.tope
-            self.tope.anterior = nuevo
+        if self.frente == None:
+            self.frente = nuevo 
+            self.ultimo = nuevo
+            nuevo.siguiete = nuevo
+            nuevo.anterior = nuevo
+        else:
+            self.frente.siguiente = nuevo
+            nuevo.anterior = self.frente
+            self.frente = nuevo
 
-        self.tope = nuevo
         self.tamaño += 1
+
+    def insert_back(self,dato):
+        nuevo = Nodo(dato)
+        if self.ultimo == None:
+            self.frente = nuevo 
+            self.ultimo = nuevo 
+            
+        else:
+            self.ultimo.anterior = nuevo
+            nuevo.siguiente = self.ultimo
+            self.ultimo = nuevo
+        self.tamaño +=1
 
     def __str__(self):
         elementos = []
-        actual = self.tope
+        actual = self.ultimo
         while actual:
             elementos.append(str(actual.dato))
             actual = actual.siguiente
-        return " <-- ".join(elementos)
+            #actual= actual.anterior
+        return " <-->".join(elementos)
+
+    
 
 # Ejemplo de uso
-pila = pilaEnlazada()
-pila.push(10)
-pila.push(20)
-pila.push(30)
+pila = pilaDoblementeEnlazada()
+pila.insert_back(10)
+pila.insert_back(20)
+pila.insert_back(30)
+pila.insert_back(50)
+
+#pila.insert_front(10)
+#pila.insert_front(20)
+#pila.insert_front(30)
+#pila.insert_front(40)
+#pila.insert_front(50)
 
 print("Pila:", pila)
